e04d5764f287a3fe2884c118afc1fd96719ceb05
Enderkries2011 <88530711+Enderkries2011@users.noreply.github.com>
2022-05-14 20:37:43 +0200

    Delete delete-junk-run-as-admin.bat

--- delete-junk-run-as-admin.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-start https://www.youtube.com/watch?v=iik25wqIuFo
-del /s /f /q c:\windows\temp\*.*
-del /s /f /q C:\WINDOWS\Prefetch
-del /s /f /q %temp%\*.*
-pause
-#start https://youtube.com/clip/UgkxT3xFu13lumJJgJJZc6GD8IPLiisnflg7
-
-
