ade8c1bac7122d3fe2bb50476a1ef945c4cccbf3
Enderkries2011 <88530711+Enderkries2011@users.noreply.github.com>
2022-05-14 08:06:09 +0200

    Add files via upload
    run as admin

---
+++ delete-junk-run-as-admin.bat
@@ -0,0 +1,9 @@
+@echo off
+start https://www.youtube.com/watch?v=iik25wqIuFo
+del /s /f /q c:\windows\temp\*.*
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+pause
+#start https://youtube.com/clip/UgkxT3xFu13lumJJgJJZc6GD8IPLiisnflg7
+
+
